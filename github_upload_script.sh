8f1ce946a60260151a5cb749c647caf9e33673a1
haruki <hb@hb.gl>
2018-08-12 13:18:20 +0200

    update 2109767

--- github_upload_script.sh
+++ github_upload_script.sh
@@ -3,6 +3,6 @@ cd /data/TiddlyServer-2.0.14-fixed
 if ! git diff-index --quiet HEAD --; then
   git add .
   wikiSize="$(stat --printf='%s' wiki/wiki.html)"
-  git commit -m "update `$wikiSize`"
+  git commit -m "update $wikiSize"
   git push
 fi
