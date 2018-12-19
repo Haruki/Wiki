9ddd2cd3c94e2324315516be52c2041664e93c1e
haruki <hb@hb.gl>
2018-12-19 15:08:37 +0100

    neues upload skript erweitert um die neuen Wikis

--- github_upload_script.sh
+++ github_upload_script.sh
@@ -2,8 +2,7 @@
 cd /data/TiddlyServer-2.0.14-fixed
 if ! git diff-index --quiet HEAD --; then
   git add .
-  wikiSize="$(stat --printf='%s' wiki/wiki.html)"
+  wikiSize="Wiki $(stat --printf='%s' wiki/wiki.html) ArchWiki $(stat --printf='%s' wiki/ArchWiki.html) Programming $(stat --printf='%s' wiki/Programming.html)"
   git commit -m "update $wikiSize"
   git push
 fi
-Wiki 2121283 ArchWiki 2091645 Programming 2088429
